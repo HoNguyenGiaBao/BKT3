85c82c8050bfcfde5650f8ff19565b1d746c9e6b
Eugen <kirishima219@gmail.com>
2022-11-30 19:21:38 +0700

    change subactivity

--- app/src/main/java/ute/application/bkt3/SubActivity.java
+++ app/src/main/java/ute/application/bkt3/SubActivity.java
@@ -3,8 +3,16 @@ package ute.application.bkt3;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ute.application.bkt3.DAO.AuthorDAO;
+import ute.application.bkt3.adapters.AuthorAdapter;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -24,5 +32,33 @@ public class SubActivity extends AppCompatActivity {
         img.setImageResource(image);
         tv.setText(name);
 
+//        setContentView(R.layout.activity_main);
+//        lv=findViewById(R.id.listViewAuthor);
+//        authorDAOArrayList =new ArrayList<>();
+//
+//        for(int i=0 ;i<bookName.length;i++){
+//            authorDAOArrayList.add(new AuthorDAO(imageBook[i],bookName[i],bookDes[i]));
+//        }
+//        authorAdapter = new AuthorAdapter(SubActivity.this,R.layout.item_author,authorDAOArrayList);
+//        lv.setAdapter(authorAdapter);
+
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(MainActivity.this,SubActivity.class);
+//                intent.putExtra("name",bookName[i]);
+//                intent.putExtra("image",imageBook[i]);
+//                intent.putExtra("des",bookDes[i]);
+//                startActivity(intent);
+//            }
+//        });
     }
+//
+//    int imageBook[]={R.drawable.conan_doyle,R.drawable.agatha_chrisite,R.drawable.victor_hugo};
+//    String bookName[]={"Arthur Conan Doyle","Agatha Christie","Victor Hugo"};
+//    String bookDes[] = {"Ông hoàng của tiểu thuyết trinh thám","Nữ hoàng của tiểu thuyết trinh thám","Nhà văn của thời đại"};
+//
+//    ArrayList<AuthorDAO> authorDAOArrayList;
+//    AuthorAdapter authorAdapter;
+//    ListView lv;
 }
